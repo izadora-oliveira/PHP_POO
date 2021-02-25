70e82f9c34389b3cfb679100fb07159b2618e515
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-02-25 19:38:36 -0300

    Update aluno.php

--- aluno.php
+++ aluno.php
@@ -14,6 +14,50 @@ class Aluno
         $this->matricula = $matricula_c;
         $this->nota1 = $nota1_c;
         $this->nota2 = $nota2_c;
-        
     }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+    }
+    public function setNota1($nota1)
+    {
+        $this->nota1 = $nota1;
+    }
+    public function setNota2($nota2)
+    {
+        $this->nota2 = $nota2;
+    }
+
+    function getNome()
+    {
+        return $this->nome;
+    }
+    function getMatricula()
+    {
+        return $this->matricula;
+    }
+    function getNota1()
+    {
+        return $this->nota1;
+    }
+    function getNota2()
+    {
+        return $this->nota2;
+    }
+
+    //métodos
+	function media($nota1, $nota2)
+	{
+		return $media = ($nota1 + $nota2) / 2;
+	}
 }
+
+//instaciar o objetos
+$objaluno = new Aluno("maria", "25022020", "7", "8");
+echo $objaluno->media();
+
