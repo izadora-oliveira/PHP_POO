0de21b6a6229637e3e73ca1be34e4590ee750264
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-08 18:24:15 -0300

    Create peixe.php

---
+++ peixe.php
@@ -0,0 +1,37 @@
+<?php
+require_once "Animal.php";
+
+class Peixe extends Animal
+{
+
+    public $caracteristica;
+
+    //método construtor
+    function __construct($nome_c, $comprimento_c, $qtdpatas_c, $cor_c, $ambiente_c, $velocidade_c, $caracteristica_c)
+    {
+    $this->nome = $nome_c;
+    $this->comprimento = $comprimento_c;
+    $this->qtdpatas = $qtdpatas_c;
+    $this->cor = $cor_c;
+    $this->ambiente = $ambiente_c;
+    $this->qtdpatas = $velocidade_c;
+    $this->caracteristica = $caracteristica_c;
+    }
+
+    //getters e setters
+    public function setCaracteristica($caracteristica)
+    {
+        $this->caracteristica = $caracteristica;
+    }
+    function getCaracteristica()
+    {
+        return $this->caracteristica;
+    }
+
+    //métodos
+    function dadospeixe()
+    {
+        
+    }
+
+}
