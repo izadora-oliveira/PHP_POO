34d60e4bb3ca7607c1ab111b9d156ca95cf724bc
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-11 16:26:40 -0300

    Update pizza.php

--- lanchonete/pizza.php
+++ lanchonete/pizza.php
@@ -2,20 +2,24 @@
 require "prato.php";
 class Pizza extends Prato
 {
-  public $molho;
-  public $recheio;
   public $borda;
+  public $recheio;
+  public $molho;
+  public $contador = 0;
 
   //fazer construtor
-  function __construct($nome, $preco, $dataDeValidade, $peso, $molho, $recheio, $borda)
+  
+  function armazena($nome, $preco, $dataDeValidade, $peso, $borda, $recheio, $molho)
   {
-    $this->nome = $nome;
-    $this->preco = $preco;
-    $this->dataDeValidade = $dataDeValidade;
-    $this->peso = $peso;
-    $this->molho = $molho;
-    $this->recheio = $recheio;
-    $this->borda = $borda;
+    $this->nome[$this->contador] = $nome;
+    $this->preco[$this->contador] = $preco;
+    $this->dataDeValidade[$this->contador] = $dataDeValidade;
+    $this->peso[$this->contador] = $peso;
+    $this->borda[$this->contador] = $borda;
+    $this->recheio[$this->contador] = $recheio;
+    $this->molho[$this->contador] = $molho;
+    $this->contador++;
+    return ($this->contador);
   }
 
   // vou deixar os set e gets com vocês
@@ -30,8 +34,24 @@ class Pizza extends Prato
 	}
 
   //métodos
+  public function imprime()
+  {
+    echo "Nome/preco/dataDeValidade/peso/borda/recheio/molho";
+    $posicao = 0;
+    for ($i = 1; $i <= $this->contador; $i++) {
+      $i = $posicao - 1;
+      echo "<br/>";
+      echo $posicao . "/" . $this->nome[$i] . "/" . $this->preco[$i] . "/" . $this->dataDeValidade[$i] . "/" . $this->peso[$i] . "/" . $this->borda[$i] . "/" . $this->recheio[$i] . "/" . $this->molho[$i];
+    }
+  }
+  public function imprimirPessoa($posicao)
+  {
+    $i = $posicao - 1;
+    echo "<br/>";
+    echo $posicao . "/" . $this->nome[$i] . "/" . $this->preco[$i] . "/" . $this->dataDeValidade[$i] . "/" . $this->peso[$i] . "/" . $this->borda[$i] . "/" . $this->recheio[$i] . "/" . $this->molho[$i];
+  }
   public function toString()
   {
-    return "Pizza{" + "molho=" + $this->molho + ", recheio=" + $this->recheio + ", borda=" + $this->borda + '}';
+    return "Pizza{" + "molho=" + $this->borda + ", recheio=" + $this->recheio + ", borda=" + $this->molho + '}';
   }
 }
